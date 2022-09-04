8bf6a263aebf0610387196d8028e9ca638c0146c
William <75191178+Willwss@users.noreply.github.com>
2022-09-04 20:36:49 -0300

    Testes com calculadora em ruby

---
+++ CalculadoraRuby/src/calculadora.rb
@@ -0,0 +1,92 @@
+class Calculadora
+
+    def soma(a, b)
+        begin
+            resultado = a + b
+            if resultado != a + b
+                return 'Nao é possivel realizar uma soma com o operador informado ou caracter apresentado'
+            else
+                resultado
+            end  
+        end
+    end
+
+'--------------------------------------------------------------------------------------------------'
+    def subtrai(a, b)
+        begin
+            resultado = a - b
+            if resultado != a-b
+                return 'Não é possivel realizar uma subtração com o operador ou caracter informado'
+            else
+                resultado.round(1)
+            end   
+        end
+    end
+'--------------------------------------------------------------------------------------------------'
+    def divide(a, b)
+        begin
+            resultado = a / b
+            if resultado != a/b
+                return 'Não é possivel relizar uma divisão com o operador ou caracter informado'
+            else
+                resultado.round(1)
+            end   
+        end
+    end
+'--------------------------------------------------------------------------------------------------'
+    def multiplica(a, b)
+        begin
+            resultado = a * b
+            if resultado != a*b
+                return 'Não é possivel relizar uma multiplicação com o operador ou caracter informadoa'
+            else
+            resultado
+            end   
+        end
+    end
+'--------------------------------------------------------------------------------------------------'   
+    def porcentagem(porce, b)
+        begin
+            resultado = ((porce * b) / 100)
+            if resultado != ((porce * b) / 100)
+                return 'Não é possivel relizar uma operaçao de porcentagem com o operador ou caracter informado'
+            else
+             resultado
+            end
+        end
+    end
+'--------------------------------------------------------------------------------------------------'
+    def potencia(a, b)
+        begin
+            resultado = a ** b
+            if resultado != a ** b
+                return 'Não é possivel relizar uma operaçao de potencia com o operador ou caracter informado'
+            else
+             resultado
+            end
+        end
+    end
+'--------------------------------------------------------------------------------------------------'
+    def raiz_q(a)
+        begin
+            resultado = Math.sqrt(a)
+            if resultado != Math.sqrt(a)
+                return 'Nao foi possivel realizar a operação raiz quadrada com o operador ou caracter informado'
+            else
+                resultado
+            end
+        end
+    end
+'--------------------------------------------------------------------------------------------------'
+    def resto_div(a, b)
+        begin
+            resultado = a % b
+            if resultado != a % b
+                return 'Não é possivel realizar uma operaçao de divisão com esse operador ou caracter informado'
+            else
+                resultado
+            end
+        end
+    end
+end
+
